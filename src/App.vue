cb0b1df032db848b8125dbb459307278e20264e3
raphael <rbensimon05@gmail.com>
2020-07-03 13:09:08 +0200

    CV Raphael

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+	<div id="app">
+		<header>
+			<h1>Raphael BENSIMON</h1>
+			<h2>Developpeur web</h2>
+		</header>
+		<div id="bar_container">
+			<img src="@/assets/img/bar.svg">
+		</div>
+		<router-view/>
+		<footer></footer>
+	</div>
+</template>
+<script type="text/javascript"></script>
+
+<style lang="less">
+@import '@/assets/css/common.less';
+#app {
+	position: relative;
+	left: 50%;
+	transform: translateX(-50%);
+	background: @white;
+	color: @blue;
+	max-width: 818px;
+	width: 100%;
+	font-size: 14px;
+	max-height: 1398px;
+}
+header {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	color: @blue;
+	text-transform: uppercase;
+	margin: 50px 0;
+}
+header h1 {
+	margin: 0;
+	font-size: 60px;
+	letter-spacing: 3px;
+	font-weight: 700;
+}
+header h2 {
+	margin: 0;
+	font-size: 25px;
+	letter-spacing: 5px;
+	font-weight: 400;
+}
+#bar_container {
+	width: 100%;
+}
+#bar_container img {
+	object-fit: cover;
+	height: 37px;
+	width: 100%;
+}
+footer {
+	height: 12px;
+	background-color: @red;
+}
+
+@media screen and (max-width: 650px) {
+	header {
+		margin: 15px 0;
+	} 
+	header h1 {
+		font-size: 30px;
+	}
+	header h2 {
+		font-size: 16px;
+	}
+}
+
+@media screen and (max-width: 350px){
+	header h1 {
+		font-size: 25px;
+	}
+	header h2 {
+		font-size: 14px;
+	}
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,207 @@
+<template>
+ 	<main id="home">
+		<div id="left_content">
+			<img id="profile_picture" src="@/assets/img/raphael.jpg">
+			<div class="content">
+				<h3>Contact</h3>
+				<p>06 ** ** ** **</p>
+				<p>rbensimon05@gmail.com</p>
+				<p>Villemomble, 93250</p>
+			</div>
+			<div class="content">
+				<h3>Diplômes</h3>
+				<p><b>Classe prépratoire web</b><br><i>ORT Montreuil 2016</i></p>
+				<p><b>Bachelor CSI</b> (Sans TOEIC)<br><i>ORT Montreuil 2016</i></p>
+			</div>
+			<div class="content">
+				<h3>Passion</h3>
+				<p>Automobile</p>
+			</div>
+		</div>
+		<div id="right_content">
+			<p id="description">En tant que développeur web, mon objectif est de comprendre les besoins des	clients, des équipes internes et de les convertir en une <b>solution web</b>, en respectant les contraintes de temps sans négliger la qualité. De nature positive,	je suis à l'écoute des <b>dernières technologies</b> web que je mets en place lors de
+			projet avec mon équipe.
+			</p>
+			<div class="content">
+				<h3>Expérience</h3>
+				<div class="exp">
+					<h4>Data4job</h4>
+					<p class="info">Développeur web front-end / Alternance / 2017 - 2020</p>
+					<p class="description">Développement web (VueJS, Meteor, NodeJS) des différentes plateformes web
+					de l'entreprise.</p>
+				</div>
+				<div class="exp">
+					<h4>ImmoJeune</h4>
+					<p class="info">Intégrateur / Stage / 6 mois / 2016</p>
+					<p class="description">Intégration web natif de landing page à l'aide de maquette graphique (HTML,CSS, JS).</p>
+				</div>
+			</div>
+			<div class="content" id="competences">
+				<h3>Compétences</h3>
+				<ul>
+					<li v-for="comp in competences">
+						<p :style="comp.bold ? 'font-weight: 700;': 'font-weight: 400;'">{{comp.name}}</p>
+						<div class="pourcent" :style="{width: comp.pourcent * 1.5 + 'px'}"></div>
+					</li>					
+				</ul>
+			</div>
+		</div>
+ 	</main>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+	data() {
+		return {
+			competences: [
+				{name: 'HTML', pourcent: 100, bold: false},
+				{name: 'CSS/LESS', pourcent: 100, bold: false},
+				{name: 'Javascript', pourcent: 80, bold: true},
+				{name: 'VueJS/Meteor', pourcent: 80, bold: true},
+				{name: 'NodeJS', pourcent: 50, bold: false},
+				{name: 'MongoDB', pourcent: 50 , bold: false}			]
+		}
+	},
+}
+</script>
+<style lang=less>
+@import '@/assets/css/common.less';
+#home {
+	display: flex;
+	color: @blue;
+}
+#home #left_content {
+	width: 30%;
+	height: 100%;
+	background-color: @grey;
+	padding: 0 40px;
+	padding-bottom: 50px;
+	display: flex;
+	flex-direction: column;
+}
+#home #left_content #profile_picture {
+	border-radius: 50%;
+	align-self: center;
+	margin-top: 30px;
+}
+#home #right_content {
+	width: 70%;
+	padding: 0 40px;
+	padding-bottom: 50px;
+	display: flex;
+	flex-direction: column;
+}
+#home #right_content #description {
+	line-height: 20px;
+	margin-top: 60px;
+	margin-bottom: 0px;
+	font-size: 14px;
+}
+#home #right_content #competences {
+	margin-top: 17px;
+}
+#home #right_content #competences ul {
+	padding: 0;
+}
+#home #right_content #competences ul li {
+	list-style: none;
+	display: flex;
+	align-items: center;
+}
+#home #right_content #competences ul li p {
+	margin: 3px 0;
+	width: 100px;
+}
+#home #right_content #competences ul li .pourcent {
+	background-color: @red;
+	height: 10px;
+}
+.content {
+	margin-top: 30px;
+}
+.content h3 {
+	text-transform: uppercase;
+	margin: 0;
+	letter-spacing: 3px;
+	font-size: 20px;
+}
+.content p {
+	margin: 10px 0;
+}
+.content .exp h4 {
+	margin-top: 10px;
+	margin-bottom: 0;
+}
+.content .exp p.info {
+	margin: 0;
+	color: @black;
+	font-style: italic;
+}
+.content .exp p.description {
+	margin-top: 0;
+}
+@media screen and (max-width: 788px){
+	#home {
+		flex-direction: column;
+	}
+	#home #left_content {
+		width: 100%;
+		flex-direction: row;
+		padding: 15px 40px;
+		flex-wrap: wrap;
+		height: auto;
+		justify-content: space-between;
+	}
+	#home #left_content #profile_picture {
+		margin-top: 0;
+		width: 120px;
+	}
+	#home #left_content .content {
+		margin: 0;
+		display: table;
+	}
+	.content p {
+		margin: 0;
+		&:first-of-type {
+			margin-top: 10px;
+		}
+		&:last-of-type  {
+			margin-top: 5px;
+		}
+	}
+	#home #right_content {
+		width: 100%;
+	}
+	#home #right_content #description {
+		margin-top: 30px;
+	}
+}
+@media screen and (max-width: 650px){
+	#home #left_content {
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
+	#home #left_content .content {
+		width: 50%;
+		text-align: right;
+		&:first-of-type {
+			margin-bottom: 20px;
+		}
+		&:nth-of-type(2){
+			text-align: left;
+		}
+	}
+	#home #left_content #profile_picture {
+		margin-bottom: 20px;
+	}
+}
+@media screen and (max-width: 350px){
+	#home #left_content {
+		flex-direction: column;
+	}
+	#home #left_content .content {
+		text-align: left;
+	}
+}
+</style>
